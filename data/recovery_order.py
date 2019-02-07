9a1f9ba402922d02a94ca329ad58f45b56288ee1
kalugin <kalugin.vlad@gmail.com>
2019-02-07 23:58:42 +0300

    add recovery order

---
+++ data/recovery_order.py
@@ -0,0 +1,9 @@
+import json
+
+order = open("/Users/kalugin/git_proj/mipt_tinkoff_hackathon_2019/data/query_text_final.txt", "r")
+answer_path = "result.json"
+
+with open(answer_path) as f:
+    rqs = json.load(f)
+    for i in rqs:
+        
