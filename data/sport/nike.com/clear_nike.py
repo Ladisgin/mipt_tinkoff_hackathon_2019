0a5cbf216233748c3937c149c67e97c7586985aa
kalugin <kalugin.vlad@gmail.com>
2019-02-07 23:22:42 +0300

    change data signature

--- data/sport/nike.com/clear_nike.py
+++ data/sport/nike.com/clear_nike.py
@@ -4,7 +4,7 @@ import xlwt
 import os
 
 
-rb = xlrd.open_workbook("v_data.xls", formatting_info=True)
+rb = xlrd.open_workbook("v_data copy.xls", formatting_info=True)
 workbook = xlwt.Workbook()
 sheet = workbook.add_sheet('PySheet1', cell_overwrite_ok=True)
 sheet.write(0, 0, "Название")
@@ -20,8 +20,9 @@ for t in range(1, sheet_r.nrows):
     name = row[0]
     name = re.sub(r"nike+", "", name)
     name = re.sub(r"Nike+", "", name)
-    name += "Nike" + name
+    name = "Nike " + name.strip()
     if(name == "Nike"): continue
+    print(name)
     sheet.write(rownum, 0, name)
 
     discr = row[1]
@@ -35,4 +36,4 @@ for t in range(1, sheet_r.nrows):
     path = re.sub(r"Nike+", "", path)
     sheet.write(rownum, 4, path)
     rownum += 1
-workbook.save("n_v_data.xls")
+workbook.save("v_data.xls")
